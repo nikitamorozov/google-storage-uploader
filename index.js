97ef6cb18709b87f20d26eba3f1322eb26c06396
Nikita Morozov <morozov.nikita@yahoo.com>
2019-08-29 01:58:28 +0300

    Added checksumm from content

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const googleCloudStorage = storage({
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.ORIGIN);
+  res.header('Access-Control-Allow-Origin', '*');
 
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
